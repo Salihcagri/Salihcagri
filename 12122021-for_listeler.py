64ef2bb5fbccfbd9015f42af8dfc9632f5598cb0
Salihcagri <95561616+Salihcagri@users.noreply.github.com>
2021-12-12 11:51:13 +0300

    Add files via upload

---
+++ 12122021-for_listeler.py
@@ -0,0 +1,9 @@
+#for döngüsü ile liste  işlemleri
+
+aylar=["ocak","şubat","mart","nisan","mayıs","haziran","temmuz","ağustos","eylül","ekim","kasım","aralık"]
+
+#for i in aylar:
+#   print(i)
+
+for i in range(12):
+    print(aylar[i]) #1. adımda print(aylar[0]) , 2.adımda print (aylar[1])
---
+++ 1212ç2021-for_döngüsü.py
@@ -0,0 +1,5 @@
+for i in range(1,10) #1 den 10 a kadar (10 dahil değil) yazar
+    print("BTK")
+    print(i)
+    print(i,"BTK")
+print("döngü bitti")
