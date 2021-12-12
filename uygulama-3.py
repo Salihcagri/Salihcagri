8caa12049d6663b1adb898af1cbc5f9ae20770f3
Salihcagri <95561616+Salihcagri@users.noreply.github.com>
2021-12-12 11:52:23 +0300

    Add files via upload

---
+++ uygulama-3.py
@@ -0,0 +1,7 @@
+#1 den 100 e kadar çift sayıları ekrana yazan python kodu
+#for i in range(1,100):
+#    if i%2==0:
+#        print(i)
+
+for i in range(0,100,2): #* dan başla ikişer ikişer art
+    print(i)
