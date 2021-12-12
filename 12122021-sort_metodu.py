98db15c35ba93f68e627bdbedd7184d47d827979
Salihcagri <95561616+Salihcagri@users.noreply.github.com>
2021-12-12 11:06:55 +0300

    Add files via upload

---
+++ 12122021-sort_metodu.py
@@ -0,0 +1,15 @@
+liste=[1,8,25,2,6,156,212,45]
+isimler=["ali","veli","ayşe","fatma"]
+print(liste)
+liste.sort() #küçükten büyüğe doğru, a dan z ye doğru sıralama yapar(artan sıralama)
+print(liste)
+isimler.sort()
+print(isimler)
+isimler.sort(reverse=True) #z den a ya doğru, büyükten küçüğe doğru sıralama yapar
+print(isimler)
+liste.sort(reverse=True)
+print(liste)
+
+#Ödev: 2 basamaklı sayıların okunuşunu yapan python programını yazınız.
+#Örnek: kullanıcı 12 girdi --> oniki
+#Örnek: kullanıcı 85 girdi --> seksenbeş
---
+++ ders3.py
@@ -0,0 +1,6 @@
+# liste=[]
+liste = ["Ahmet", 23 ,"5052231515"]# indeksler 0 dan başlar
+print(liste)
+print(liste[0])#  0. indeksdeki eleman yazdırıldı
+liste.append("öğrenci")#listenin sonuna ekler
+print(liste)
